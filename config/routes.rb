27a9c6b02715d7a133216b6a29b365d5bd088374
Joch <xoshdev@gmail.com>
2023-09-21 22:13:21 -0300

    Deployment Desafío 772.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "posts#index"
+end
